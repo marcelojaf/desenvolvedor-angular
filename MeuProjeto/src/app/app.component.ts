f81e93cda8643379f08b7d43d7a79c9a2fc85ebf
Marcelo Amador <marcelojaf@gmail.com>
2023-12-13 15:26:54 -0300

    Prototipo finalizado

--- MeuProjeto/src/app/app.component.ts
+++ MeuProjeto/src/app/app.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: "./app.component.html",
   styles: [],
 })
 export class AppComponent {
-  title = 'MeuProjeto';
+  title = "MeuProjeto";
 }
